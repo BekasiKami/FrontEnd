996c468146558ae2638e8b2086a98e30a8a2465f
Fatih Alsena <fatihalsena@gmail.com>
2023-12-04 16:26:25 +0700

    category

--- resources/views/partials/category.blade.php
+++ resources/views/partials/category.blade.php
@@ -1,27 +1,27 @@
-<div class="content-center mt-16">
+<div class="content-center mt-20 max-mobile:w-auto">
     <div
-        class="mx-auto w-[450px] text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        class="mx-auto w-[450px] text-sm font-medium text-center text-gray-500 border-b border-gray-200 max-mobile:w-auto">
         <ul class="flex flex-wrap -mb-px">
             <li class="mx-2">
                 <a href="/home"
-                    class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+                    class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active max-mobile:text-xs"
                     aria-current="page">Semua</a>
             </li>
             <li class="mx-2">
                 <a href="/loker"
-                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Loker</a>
+                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 max-mobile:text-xs">Loker</a>
             </li>
             <li class="mx-2">
                 <a href="/event"
-                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Events</a>
+                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 max-mobile:text-xs">Events</a>
             </li>
             <li class="mx-2">
                 <a href="/wisata"
-                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Wisata</a>
+                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 max-mobile:text-xs">Wisata</a>
             </li>
             <li class="mx-2">
                 <a
-                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Kuliner</a>
+                    class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 max-mobile:text-xs">Kuliner</a>
             </li>
         </ul>
     </div>
