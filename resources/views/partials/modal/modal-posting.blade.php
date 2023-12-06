4188f1b13a95692a77b7c8b79a9e2c0233fbfa03
Fatih <fatih@192.168.1.11>
2023-12-06 11:54:13 +0700

    button post

--- resources/views/partials/modal/modal-posting.blade.php
+++ resources/views/partials/modal/modal-posting.blade.php
@@ -1,25 +1,63 @@
 <!-- The Modal -->
 <div id="modal-posting"
-    class="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center">
+    class="hidden fixed top-0 left-0 z-40 w-screen h-screen bg-black/70 flex justify-center items-center">
 
     <!-- The close button -->
     <a class="fixed z-90 top-32 right-8 text-white text-5xl font-bold" href="javascript:void(0)"
         onclick="closeModal()">&times;</a>
 
     <!-- A big image will be displayed here -->
-    <div class="bg-white w-[1300px] h-auto inline-flex" id="posting">
+    <div class="bg-white w-[1200px] h-auto inline-flex" id="posting">
         <img id="modal-img" class="max-w-[800px] max-h-[600px]" />
-        <div class="w-[450px] h-[50px] p-2.5 justify-between inline-flex">
+        <div class="w-full h-[50px] mt-5 px-5 justify-between inline-flex">
             <div class="justify-center items-center gap-5 flex">
-                <img class="w-[30px] h-[30px] rounded-[250px] border border-zinc-600" src="assets/images/silverash-profile.svg" />
-                <div class="text-center text-zinc-700 text-sm font-normal">
-                    silver.ash
+                <img class="w-[30px] h-[30px] rounded-[250px] border border-zinc-600"
+                    src="/assets/images/jessica.svg" />
+                <div class="">
+                    <p class="text-zinc-700 text-sm font-normal">
+                        Kevin86
+                    </p>
+                    <p class="text-zinc-700 text-opacity-60 text-xs font-normal">
+                        2 jam yang lalu
+                    </p>
                 </div>
             </div>
-            <div class="justify-center items-center gap-5 flex">
-                <button class="bg-transparent cursor-pointer border-0">
-                    <img src="assets/images/three-dots.png" alt="" />
-                </button>
+
+            <button id="dropdownMenuIconHorizontalButton" data-dropdown-toggle="dropdownDotsHorizontal"
+                class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                type="button">
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                    viewBox="0 0 16 3">
+                    <path
+                        d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                </svg>
+            </button>
+
+            <!-- Dropdown menu -->
+            <div id="dropdownDotsHorizontal"
+                class="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownMenuIconHorizontalButton">
+                    <li>
+                        <a href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tambah
+                            ke simpan</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Laporkan</a>
+                    </li>
+                    <li>
+                        <a href="#"
+                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sembunyikan
+                            postingan</a>
+                    </li>
+                </ul>
+                {{-- <div class="py-2">
+                        <a href="#"
+                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Separated
+                            link</a>
+                    </div> --}}
             </div>
         </div>
     </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,7 +7,7 @@
 
 <body>
     @include('partials.modal.modal-posting')
-    @include('partials.navbar')
+    {{-- @include('partials.navbar') --}}
 
     <main>
         <div class="container mt-20">
@@ -54,11 +54,13 @@
                             </div>
                             <div
                                 class="pb-2.5 w-full justify-start flex text-black text-xs font-normal font-'Poppins' leading-4">
-                                Kevin Sanjaya</div>
+                                Kevin Sanjaya
+                            </div>
                             <div class="w-full justify-between items-center inline-flex">
-                                <div class="text-black text-sm font-normal font-'Poppins' leading-5">6 Postingan</div>
+                                <div class="text-black text-sm font-normal font-'Poppins' leading-5">12 Postingan</div>
                                 <div class="text-black text-sm font-normal font-'Poppins' leading-5 sm:justify-end">3
-                                    Tersimpan</div>
+                                    Tersimpan
+                                </div>
                             </div>
                         </div>
                     </div>
